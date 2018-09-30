190a1ce1e52095561e19196efe67e02d3493c84b
unknown <benzhang1993@hotmail.com>
2018-09-30 14:40:15 -0700

    added threading and implemented connect button

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -11,10 +11,14 @@
 #include "resource.h"
 #include <conio.h>
 #include <iostream>
+#include <thread>
 
 static TCHAR Name[] = TEXT("Comm Shell");
 char str[80] = "";
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+DWORD WINAPI readLoop(LPVOID);
+VOID writeLoop(MSG);
+BOOL connect();
 
 #pragma warning (disable: 4096)
 
@@ -30,12 +34,12 @@ HDC hdc;
 static unsigned x_cordinate = 0;
 static unsigned y_cordinate = 0;
 int ch;
+MSG Msg;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInstance,
 	LPSTR lspszCmdParam, int nCmdShow)
 {
 	HWND hwnd;
-	MSG Msg;
 	WNDCLASSEX Wcl;
 
 	OutputDebugStringA("hello");
@@ -62,6 +66,89 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInstance,
 		600, 400, NULL, NULL, hInst, NULL);
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
+	
+	DWORD threadId;
+	CreateThread(NULL, 0, readLoop, (LPVOID) hwnd, 0, &threadId);
+	writeLoop(Msg);
+
+	return Msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT Message,
+	WPARAM wParam, LPARAM lParam)
+{
+	int response;
+	PAINTSTRUCT paintstruct;
+
+	switch (Message)
+	{
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDM_COMMANDMODE:
+			cc.dwSize = sizeof(COMMCONFIG);
+			cc.wVersion = 0x100;
+			GetCommConfig(hComm, &cc, &cc.dwSize);
+			if (!CommConfigDialog(lpszCommName, hwnd, &cc))
+				break;
+			break;
+		case IDM_CONNECTMODE:
+			connect();
+			break;
+		case IDM_DISCONNECT:
+			// TODO: Disconnect
+			break;
+		}
+		break;
+
+	case WM_CHAR:	// Process keystroke
+		hdc = GetDC(hwnd);			 // get device context
+
+		// TODO: IMPLEMENT DELETE BUTTON
+
+		sprintf_s(str, "%c", (char)wParam); // Convert char to string
+		//TextOut(hdc, 10 * x_cordinate, y_cordinate, str, strlen(str)); // output character	
+		//x_cordinate++; // increment the screen x-coordinate
+		WriteFile(hComm, str, 1, &written, NULL);
+		ReleaseDC(hwnd, hdc); // Release device context
+		break;
+
+	case WM_PAINT:		// Process a repaint message
+		hdc = BeginPaint(hwnd, &paintstruct); // Acquire DC
+		TextOut(hdc, 0, 0, str, strlen(str)); // output character
+		EndPaint(hwnd, &paintstruct); // Release DC
+		break;
+
+	case WM_DESTROY:	// Terminate program
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, Message, wParam, lParam);
+	}
+	return 0;
+}
+
+DWORD WINAPI readLoop(LPVOID hwnd) {
+	while (1) {
+		ReadFile(hComm, buffer, sizeof(buffer), &read, NULL);
+		if (read) {
+			hdc = GetDC((HWND) hwnd);
+			TextOut(hdc, 10 * x_cordinate, y_cordinate, buffer, strlen(buffer)); // output character	
+			x_cordinate++; // increment the screen x-coordinate
+			ReleaseDC((HWND) hwnd, hdc); // Release device context
+		}
+	}
+}
+
+VOID writeLoop(MSG Msg) {
+	while (GetMessage(&Msg, NULL, 0, 0))
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+}
+
+BOOL connect() {
 
 	// Initializes Com Port
 	if ((hComm = CreateFile(lpszCommName, GENERIC_READ | GENERIC_WRITE, 0,
@@ -108,79 +195,4 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInstance,
 	timeouts.ReadTotalTimeoutConstant = 1;
 	timeouts.WriteTotalTimeoutMultiplier = 1;
 	timeouts.WriteTotalTimeoutConstant = 1;
-
-	// reads input from Serial Port
-	while (1) {
-		ReadFile(hComm, buffer, sizeof(buffer), &read, NULL);
-		if (read) {
-			hdc = GetDC(hwnd);
-			OutputDebugStringA(buffer);
-			TextOut(hdc, 10 * x_cordinate, y_cordinate, buffer, strlen(buffer)); // output character	
-			x_cordinate++; // increment the screen x-coordinate
-			ReleaseDC(hwnd, hdc); // Release device context
-		}
-	}
-
-	while (GetMessage(&Msg, NULL, 0, 0))
-	{
-		TranslateMessage(&Msg);
-		DispatchMessage(&Msg);
-	}
-
-
-	return Msg.wParam;
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT Message,
-	WPARAM wParam, LPARAM lParam)
-{
-	int response;
-	PAINTSTRUCT paintstruct;
-
-	switch (Message)
-	{
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDM_COMMANDMODE:
-			cc.dwSize = sizeof(COMMCONFIG);
-			cc.wVersion = 0x100;
-			GetCommConfig(hComm, &cc, &cc.dwSize);
-			if (!CommConfigDialog(lpszCommName, hwnd, &cc))
-				break;
-			break;
-		case IDM_CONNECTMODE:
-			// TODO: Make connection
-			break;
-		case IDM_DISCONNECT:
-			// TODO: Disconnect
-			break;
-		}
-		break;
-
-	case WM_CHAR:	// Process keystroke
-		hdc = GetDC(hwnd);			 // get device context
-
-		// TODO: IMPLEMENT DELETE BUTTON
-
-		sprintf_s(str, "%c", (char)wParam); // Convert char to string
-		//TextOut(hdc, 10 * x_cordinate, y_cordinate, str, strlen(str)); // output character	
-		//x_cordinate++; // increment the screen x-coordinate
-		WriteFile(hComm, str, 1, &written, NULL);
-		ReleaseDC(hwnd, hdc); // Release device context
-		break;
-
-	case WM_PAINT:		// Process a repaint message
-		hdc = BeginPaint(hwnd, &paintstruct); // Acquire DC
-		TextOut(hdc, 0, 0, str, strlen(str)); // output character
-		EndPaint(hwnd, &paintstruct); // Release DC
-		break;
-
-	case WM_DESTROY:	// Terminate program
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hwnd, Message, wParam, lParam);
-	}
-	return 0;
 }
